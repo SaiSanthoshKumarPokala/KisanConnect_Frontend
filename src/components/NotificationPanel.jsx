ed59bac314901fe66c4ce1ff2459884b6511c316
SaiSanthoshKumarPokala <saisanthoshkumarpokala@gmail.com>
2026-09-20 18:19:48 +0530

    Small bug in payment resolved

--- src/components/NotificationPanel.jsx
+++ src/components/NotificationPanel.jsx
@@ -336,6 +336,7 @@ export default function NotificationPanel() {
             const verify = await axios.post("/api/buy/verify", {
               razorpay_order_id:   response.razorpay_order_id,
               razorpay_payment_id: response.razorpay_payment_id,
+              razorpay_signature:  response.razorpay_signature,
             });
 
             if (verify.data.success) {
